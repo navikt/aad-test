f5ce8a03e164ed8b72615efbfd6fae9d814fb565
Eirik <eiriksletteberg@gmail.com>
2018-08-09 17:10:35 +0200

    Update with SAML token exchange

--- index.js
+++ index.js
@@ -38,6 +38,10 @@ async function runApp() {
     })));
 
     app.get('/gosys/callback', async (req, res) => {
+        if (req.query.error) {
+            res.send(`<h1>Authentication Error</h1><p>${req.query.error_description}</p>`)
+            return;
+        }
         const code = req.query.code;
         const tokenRes = await fetch(token_endpoint, {
             method: 'POST',
@@ -53,9 +57,53 @@ async function runApp() {
             })
         });
         const data = await tokenRes.json();
+        const idToken = data.id_token;
+
+        let text = '';
+        try {
+            const stsResponse = await fetch(config.stsUrl, {
+                method: 'post',
+                headers: {
+                    'Content-Type': 'text/xml',
+                    'SoapAction': 'http://docs.oasis-open.org/ws-sx/ws-trust/200512/RST/Issue'
+                },
+                body: `
+<soap:Envelope xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">
+<soap:Header>
+<wsse:Security soap:mustUnderstand="1" xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd">
+<wsse:UsernameToken xmlns:wsu="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-utility-1.0.xsd" wsu:Id="UsernameToken-76db6c8f-9de8-4cdf-a23d-3335676df8e7">
+<wsse:Username>${config.systemUser}</wsse:Username>
+<wsse:Password Type="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-username-token-profile-1.0#PasswordText">${config.systemPassword}</wsse:Password>
+</wsse:UsernameToken>
+</wsse:Security>
+</soap:Header>
+<soap:Body>
+<wst:RequestSecurityToken xmlns:wst="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
+<wst:SecondaryParameters xmlns:wst="http://docs.oasis-open.org/ws-sx/ws-trust/200512">
+        <wst:TokenType>http://docs.oasis-open.org/wss/oasis-wss-saml-token-profile-1.1#SAMLV2.0</wst:TokenType>
+</wst:SecondaryParameters>
+<wst:KeyType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Bearer</wst:KeyType>
+<wst:RequestType>http://docs.oasis-open.org/ws-sx/ws-trust/200512/Issue</wst:RequestType>
+<wst:OnBehalfOf>
+<wsse:BinarySecurityToken xmlns:wsse="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd" EncodingType="http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-soap-message-security-1.0#Base64Binary" ValueType="urn:ietf:params:oauth:token-type:jwt">${idToken}</wsse:BinarySecurityToken>
+</wst:OnBehalfOf>
+<wst:Renewing Allow="false"/>
+</wst:RequestSecurityToken>
+</soap:Body>
+</soap:Envelope>`
+            });
+
+            const text = await stsResponse.text();
+        } catch (err) {
+            text = 'Could not get SAML token: ' + err;
+        }
+
         res.send(`
 <h1>Result</h1>
 ${Object.keys(data).map(key => `<b>${escape(key)}:</b> ${escape(data[key])}<br />`).join('')}
+
+<h2>SAML</h2>
+<pre>${escape(text)}</pre>
 `);
     });
 
